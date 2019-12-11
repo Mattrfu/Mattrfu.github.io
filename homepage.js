c3cc8046afeefa12a1248c7f373958012bc81c8d
Mattrfu <31283200+Mattrfu@users.noreply.github.com>
2019-12-11 01:07:20 -0500

    First full update
    This is the first full iteration of the website

--- homepage.js
+++
@@ -1,6 +0,0 @@
-function sendMail() {
-    // var link = "mailto:matthewfu@college.harvard.edu"
-            //  + "&subject=" + escape("Subject: ".concat(subject, " - ", name))
-            //  + "&body=" + escape(document.getElementById(text).value);
-    document.getElementById("Send").innerHTML = "Sent!";
-}
